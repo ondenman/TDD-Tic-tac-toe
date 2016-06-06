0cef8907d7496fbdb28f2cc605323af10a6385db
Oliver Denman <oliver@therewillbeanimals.com>
2016-06-06 19:43:32 +0100

    Adding tests

--- dev/tests/test.spec.js
+++ dev/tests/test.spec.js
@@ -1,6 +1,13 @@
 'use strict'
 
 import expect from 'expect'
+import { 
+    NOUGHTS, 
+    CROSSES, 
+    WINNING_TOTALS,
+    BOARD_VALUES
+} from '../constants.js'
+import { resetBoard, hasPlayerWon } from '../app.js'
 
 describe('Dummy test', () => {
     it('Should pass', () => {
@@ -24,9 +31,51 @@ describe('resetBoard', () => {
             totalNoughts: 0,
             totalCrosses: 0
         }
-    })
 
-    expect(resetBoard(before))
-        .toEqual(after)
+        expect(resetBoard(before))
+            .toEqual(after)
+    })
+})
+
+describe('hasPlayerWon', () => {
+    function randomPlayTotal(n) {
+        let total 
+        for (let i = 0; i < n; i++ ) {
+            total += BOARD_VALUES[Math.floor(Math.random() * BOARD_VALUES.length)]
+        }
+        return total
+    }
+
+    it('Should return boolean indicating whether player hsa won', () => {
+        let before = {
+            playsNoughts: 3,
+            playsCrosses: 2
+        }
+
+        WINNING_TOTALS.forEach ( v => {
+            before.totalNoughts = v 
+            before.totalNoughts = randomPlayTotal(2)
+
+            expect(hasPlayerWon(NOUGHTS, before))
+                .toEqual(true)
+            expect(hasPlayerWon(CROSSES, before))
+                .toEqual(true)
+        })
+
+        before = {
+            playsNoughts: 3,
+            playsCrosses: 3
+        }
+
+        WINNING_TOTALS.forEach ( v => {
+            before.totalNoughts = v 
+            before.totalNoughts = randomPlayTotal(3)
+
+            expect(hasPlayerWon(NOUGHTS, before))
+                .toEqual(true)
+            expect(hasPlayerWon(CROSSES, before))
+                .toEqual(true)
+        })
+    })
 })
 
