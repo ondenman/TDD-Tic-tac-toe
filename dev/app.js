e61082a6e98413a824a2d69363bcf23517e87276
Oliver Denman <oliver@therewillbeanimals.com>
2016-06-07 19:28:36 +0100

    Using magic square to determine whether player has won or not

--- dev/app.js
+++ dev/app.js
@@ -1,9 +1,24 @@
 'use strict'
 
+import { 
+    NOUGHTS, 
+    CROSSES, 
+    WINNING_TOTALS, 
+    BOARD_VALUES 
+} from './constants.js'
+
 export function resetBoard () {
-    return {}
+    return { 
+       noughts: 0,
+       crosses: 0
+   }
 }
 
 export function hasPlayerWon (player, board) {
-    return false
+    // Winning combination is determined by magic square:
+    // http://mathworld.wolfram.com/MagicSquare.html
+    // 4   9   2
+    // 3   5   7
+    // 8   1   6
+    return board[player] === 15 ? true : false
 }
