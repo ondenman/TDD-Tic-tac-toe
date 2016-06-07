59ee43bf6f8dbed8cd4fcfecff65e80ef8477ade
Oliver Denman <oliver@therewillbeanimals.com>
2016-06-07 23:22:35 +0100

    Testing state arrays.

--- dev/tests/test.spec.js
+++ dev/tests/test.spec.js
@@ -7,40 +7,79 @@ import {
     WINNING_TOTALS,
     BOARD_VALUES
 } from '../constants.js'
-import { resetBoard, hasPlayerWon } from '../app.js'
+import game from '../app.js'
 
-describe('Dummy test', () => {
-    it('Should pass', () => {
-        expect(true)
-            .toEqual(true)
-    })
-})
 
 describe('resetBoard', () => {
     it('Should reset plays and totals', () => {
         const before =  {
-            noughts : 3,
-            crosses : 6,
+            noughts : [4, 9, 2],
+            crosses : [3, 6, 7],
+            player: CROSSES
         }
 
         const after = {
-            noughts : 0,
-            crosses : 0,
+            noughts : [],
+            crosses : [],
+            player: NOUGHTS
         }
 
-        expect(resetBoard(before))
+        expect(game.resetBoard(before))
             .toEqual(after)
+    })
+})
+
+describe('addSquareToPlayer', () => {
+    it('should add square value to player array', () => {
+        const before = {
+            noughts: [],
+            crosses: [],
+            player: CROSSES
+        }
+        const after = Object.assign(before, { crosses: [3] })
+        expect(game.addSquareToPlayer(before, 3)).toEqual(after)    
     })
 })
 
 describe('hasPlayerWon', () => {
     it('Should return a boolean indicating whether player has won', () => {
         const before =  {
-            noughts : 15,
-            crosses : 8
+            noughts : [4, 9, 2, 8],
+            crosses : [6, 7, 1],
+            player: NOUGHTS
         }
-        expect(hasPlayerWon(NOUGHTS, before)).toEqual(true)
-        expect(hasPlayerWon(CROSSES, before)).toEqual(false)
+        expect(game.hasPlayerWon(before)).toEqual(true)
+
+        before.player = CROSSES
+        expect(game.hasPlayerWon(before)).toEqual(false)
+
+        before.player = NOUGHTS
+        before.noughts = [4, 9]
+        expect(game.hasPlayerWon(before)).toEqual(false)
     })
 })
 
+describe('isGameDraw', () => {
+    it('Should return boolean indicating if game is drawn or not', () => {
+        const before = {
+            noughts : [3, 5, 2, 1, 6],
+            crosses: [4, 9, 8, 7],
+            player: NOUGHTS
+        }
+        expect(game.isGameDraw(before)).toEqual(true)
+    })
+})
+
+
+describe('togglePlayer', () => {
+    const before = {
+        player: NOUGHTS
+    }
+    const after = {
+        player: CROSSES
+    }
+
+    it('should toggle the current player to crosses if last player was noughts', () => {
+        expect(game.togglePlayer(after)).toEqual(before)
+    })
+})
